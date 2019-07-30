be078a960a10bdfe109a6df95c50dcaf74f7e824
AlbertCoca <coka.albert@gmail.com>
2019-07-30 22:18:42 +0200

    Added easy installation scripts

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install vimrc configuration
+cp .vimrc $HOME/.vimc
+
+# Install nice fonts for vim-airline
+sudo apt install -y fonts-powerline
+
+# TODO: Need to upload the colorscheme molokai to the repo and add it to vim
+
+# NOTE: At this point you should open vim and run PluginInstall then after this
+# you can compile youcompleteme
---
+++ install_youcompleteme.sh
@@ -0,0 +1,7 @@
+
+# Install cmake dependency from youcompleteme plugin
+sudo apt-get install cmake
+
+# Compile youcomplete me
+python3 $HOME/.vim/bundle/youcompleteme/install.py
+
