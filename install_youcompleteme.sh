13be8db24b28eec3556e64d9ed1c766a86c0b0ba
AlbertCoca <albert.coca@mitigasolutions.com>
2020-01-13 18:12:18 +0100

    Added python3-dev package as dependency of youcompleteme

--- install_youcompleteme.sh
+++ install_youcompleteme.sh
@@ -1,6 +1,7 @@
 
 # Install cmake dependency from youcompleteme plugin
 sudo apt-get install cmake
+sudo apt-get install python3-dev
 
 # Compile youcomplete me
 python3 $HOME/.vim/bundle/youcompleteme/install.py
